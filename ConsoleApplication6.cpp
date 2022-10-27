d21f5f47252be65c237e6df8d53647a1bbb37bdf
Zver <Zver@ZVERDVD-7IKBG2I>
2022-10-28 00:44:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,41 @@
+﻿// ConsoleApplication6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "iostream"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src1;
+	src1 = imread("vik.jpg", cv::IMREAD_COLOR);
+	namedWindow("Original image", cv::WINDOW_NORMAL);
+	imshow("Original image", src1);
+
+	Mat gray, edge, draw;
+	cvtColor(src1, gray, cv::COLOR_BGR2GRAY);
+
+	Canny(gray, edge, 25, 60, 3);
+
+	edge.convertTo(draw, CV_8U);
+	namedWindow("image", cv::WINDOW_NORMAL);
+	imshow("image", draw);
+
+	waitKey(0);
+	return 0;
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
